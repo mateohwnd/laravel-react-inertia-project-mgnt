d7973e86a740ed442205519d68b7cb91a8c82176
mateohwnd <christianhwnd@gmail.com>
2024-06-01 02:45:16 +0800

    add pagination to projects page

--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -1,3 +1,4 @@
+import Pagination from "@/Components/Pagination";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head, Link } from "@inertiajs/react";
 
@@ -64,6 +65,7 @@ export default function Index({ auth, projects }) {
                   ))}
                 </tbody>
               </table>
+              <Pagination links={projects.meta.links} />
             </div>
           </div>
         </div>
