66ba13816cbc4295e36864c3bbf8c4d9c27cbb59
mateohwnd <christianhwnd@gmail.com>
2024-06-01 03:18:56 +0800

    added textinput and selectInput component to projects table

--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -1,5 +1,11 @@
 import Pagination from "@/Components/Pagination";
+import SelectInput from "@/Components/SelectInput";
+import TextInput from "@/Components/TextInput";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import {
+  PROJECT_STATUS_CLASS_MAP,
+  PROJECT_STATUS_TEXT_MAP,
+} from "@/constansts.jsx";
 import { Head, Link } from "@inertiajs/react";
 
 export default function Index({ auth, projects }) {
@@ -19,27 +25,44 @@ export default function Index({ auth, projects }) {
           <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
             <div className="p-6 text-gray-900 dark:text-gray-100">
               <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500"></thead>
                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500">
                   <tr className="text-nowrap">
-                    <th className="px-3 py-3">ID</th>
-                    <th className="px-3 py-3">Image</th>
-                    <th className="px-3 py-3">Name</th>
-                    <th className="px-3 py-3">Status</th>
-                    <th className="px-3 py-3">Create Date</th>
-                    <th className="px-3 py-3">Due Date</th>
-                    <th className="px-3 py-3">Created By</th>
-                    <th className="px-3 py-3 text-right">Actions</th>
+                    <th className="px-3 py-3"></th>
+                    <th className="px-3 py-3"></th>
+                    <th className="px-3 py-3">
+                      <TextInput />
+                    </th>
+                    <th className="px-3 py-3">
+                      <SelectInput />
+                    </th>
+                    <th className="px-3 py-3"></th>
+                    <th className="px-3 py-3"></th>
+                    <th className="px-3 py-3"></th>
+                    <th className="px-3 py-3"></th>
                   </tr>
                 </thead>
                 <tbody>
                   {projects.data.map((project) => (
-                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                    <tr
+                      className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                      key={project.id}
+                    >
                       <td className="px-3 py-2">{project.id}</td>
                       <td className="px-3 py-2">
                         <img src={project.image_path} style={{ width: 60 }} />
                       </td>
                       <td className="px-3 py-2">{project.name}</td>
-                      <td className="px-3 py-2">{project.status}</td>
+                      <td className="px-3 py-2">
+                        <span
+                          className={
+                            "px-2 py-1 rounded text-white " +
+                            PROJECT_STATUS_CLASS_MAP[project.status]
+                          }
+                        >
+                          {PROJECT_STATUS_TEXT_MAP[project.status]}
+                        </span>
+                      </td>
                       <td className="px-3 py-2 text-nowrap">
                         {project.created_at}
                       </td>
