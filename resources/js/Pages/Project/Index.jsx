9274105167b3c3900a672eeee7c1af8fd2603d10
mateohwnd <christianhwnd@gmail.com>
2024-06-03 23:15:46 +0800

    sort table by column name functionality implemented, and componetize tableheading

--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -1,3 +1,4 @@
+import { ChevronUpIcon, ChevronDownIcon } from "@heroicons/react/16/solid";
 import Pagination from "@/Components/Pagination";
 import SelectInput from "@/Components/SelectInput";
 import TextInput from "@/Components/TextInput";
@@ -7,6 +8,7 @@ import {
   PROJECT_STATUS_TEXT_MAP,
 } from "@/constansts.jsx";
 import { Head, Link, router } from "@inertiajs/react";
+import TableHeading from "@/Components/TableHeading";
 
 export default function Index({ auth, projects, queryParams = null }) {
   queryParams = queryParams || {};
@@ -59,37 +61,47 @@ export default function Index({ auth, projects, queryParams = null }) {
                 <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                   <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500">
                     <tr className="text-nowrap">
-                      <th
-                        onClick={(e) => sortChanged("id")}
-                        className="px-3 py-3"
+                      <TableHeading
+                        name="id"
+                        sort_field={queryParams.sort_field}
+                        sort_direction={queryParams.sort_direction}
+                        sortChanged={sortChanged}
                       >
                         ID
-                      </th>
+                      </TableHeading>
                       <th className="px-3 py-3">Image</th>
-                      <th
-                        onClick={(e) => sortChanged("name")}
-                        className="px-3 py-3"
+                      <TableHeading
+                        name="name"
+                        sort_field={queryParams.sort_field}
+                        sort_direction={queryParams.sort_direction}
+                        sortChanged={sortChanged}
                       >
                         Name
-                      </th>
-                      <th
-                        onClick={(e) => sortChanged("status")}
-                        className="px-3 py-3"
+                      </TableHeading>
+                      <TableHeading
+                        name="status"
+                        sort_field={queryParams.sort_field}
+                        sort_direction={queryParams.sort_direction}
+                        sortChanged={sortChanged}
                       >
                         Status
-                      </th>
-                      <th
-                        onClick={(e) => sortChanged("created_at")}
-                        className="px-3 py-3"
+                      </TableHeading>
+                      <TableHeading
+                        name="created_at"
+                        sort_field={queryParams.sort_field}
+                        sort_direction={queryParams.sort_direction}
+                        sortChanged={sortChanged}
                       >
                         Create Date
-                      </th>
-                      <th
-                        onClick={(e) => sortChanged("due_date")}
-                        className="px-3 py-3"
+                      </TableHeading>
+                      <TableHeading
+                        name="due_date"
+                        sort_field={queryParams.sort_field}
+                        sort_direction={queryParams.sort_direction}
+                        sortChanged={sortChanged}
                       >
                         Due Date
-                      </th>
+                      </TableHeading>
                       <th className="px-3 py-3">Created By</th>
                       <th className="px-3 py-3 text-right">Actions</th>
                     </tr>
@@ -140,7 +152,7 @@ export default function Index({ auth, projects, queryParams = null }) {
                           <img src={project.image_path} style={{ width: 60 }} />
                         </td>
                         <td className="px-3 py-2">{project.name}</td>
-                        <td className="px-3 py-2">
+                        <td className="px-3 py-2 text-nowrap">
                           <span
                             className={
                               "px-2 py-1 rounded text-white " +
